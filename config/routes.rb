b50d9ace570db247db821973d0375d98ac3890b5
Ezequiel <epuyosdacosta@gmail.com>
2022-11-09 15:26:50 +0100

    Class completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # # Read all
+  # get "restaurants", to: "restaurants#index"
+  # # Create
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # # Read one
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # # Update
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+  # # Delete
+  # delete "restaurants/:id", to: "restaurants#destroy"
+
+  # resources :restaurants, only: [:index, :show]
+
+  # resources :restaurants, except: [:index, :show]
+
+  resources :restaurants
 end
